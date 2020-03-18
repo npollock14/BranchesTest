4e22c225ed12ac2e9b2eb366e25fe03d07d699a9
npollock14 <33988166+npollock14@users.noreply.github.com>
2020-03-17 18:32:00 -0700

    added stuff

---
+++ BranchesTest/src/BigBrain.java
@@ -0,0 +1,5 @@
+
+public class BigBrain {
+	public static String yolo = "yolo";
+
+}
--- BranchesTest/src/BranchTestMain.java
+++ BranchesTest/src/BranchTestMain.java
@@ -3,6 +3,8 @@ public class BranchTestMain {
 
 	public static void main(String[] args) {
 		System.out.println("My name toad");
+		System.out.println("New Addition");
+		System.out.println(BigBrain.yolo);
 
 	}
 
